2ef5ffda74913496751e15d05827261c354ab93a
psr <gt123456>
2017-10-17 14:51:48 +0800

    初始化应用类项目

---
+++ customize/src/main/java/com/gt/customize/CustomizeApplication.java
@@ -0,0 +1,12 @@
+package com.gt.customize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomizeApplication.class, args);
+	}
+}
