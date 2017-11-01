c01b7c0d37502a419a67583e85ccaf8e00734ddd
lixaoqiang <gzhidao1010@gmail.com>
2017-11-01 17:45:35 +0800

    路由动画

--- cli-h5/parkMap/src/App.vue
+++ cli-h5/parkMap/src/App.vue
@@ -1,14 +1,51 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <router-view class="child-view"></router-view>
+    </transition>
   </div>
 </template>
 <script>
-export default {
-  name: 'app',
-}
+  export default {
+    name: 'app',
+    data() {
+      return {
+        transitionName: 'slide-left'
+      }
+    },
+    watch: {
+      '$route' (to, from) {
+        const toDepth = to.path.split('/').length
+        const fromDepth = from.path.split('/').length
+        this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
+      }
+    }
+  }
+
 </script>
 
-<style>
+<style scoped>
+  .child-view {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transition: all .5s cubic-bezier(.55, 0, .1, 1);
+  }
+
+  .slide-left-enter,
+  .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(30px, 0);
+    transform: translate(30px, 0);
+  }
+
+  .slide-left-leave-active,
+  .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-30px, 0);
+    transform: translate(-30px, 0);
+  }
 
 </style>
