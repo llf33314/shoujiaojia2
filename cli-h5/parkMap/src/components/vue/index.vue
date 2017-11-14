81965ee2bd35d91a33b2d87ccd4a61b09f67a5be
lixaoqiang <gzhidao1010@gmail.com>
2017-11-14 09:26:31 +0800

    h5

--- cli-h5/parkMap/src/components/vue/index.vue
+++ cli-h5/parkMap/src/components/vue/index.vue
@@ -451,7 +451,7 @@
       this._wx.getWxSDK(this.$route.params.busId, {
         title: '景区地图',
         link: window.location.href,
-        imgUrl: '//maint.deeptel.com.cn/upload//image/3/goodtom/3/20171030/6D19FD6D60C4B424348F07EFE9B3408C.jpg'
+        imgUrl: 'http://maint.deeptel.com.cn/upload//image/3/goodtom/3/20171030/6D19FD6D60C4B424348F07EFE9B3408C.jpg'
       })
 
       touch.on(document.getElementById('mapCstyle'), 'pinchstart', (e) => {
